74da05266792626ecf039b8bf5d7224f7f61e95b
opee2012 <jborth2010@gmail.com>
2023-11-07 15:02:56 -0500

    Added comments

--- server/routes/login-routes.js
+++ server/routes/login-routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// GET login information
 router.get('/', (req, res) => {
     res.json({
         message: 'GET login'})
 });
 
+// router.post('/', (req, res) => {});
+
+// router.put('/', (req, res) => {});
+
+// router.delete('/', (req, res) => {});
 
 module.exports = router;
