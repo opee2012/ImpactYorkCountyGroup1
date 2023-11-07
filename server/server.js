1caeb0a280432e5014f85714226044ba72215c88
opee2012 <jborth2010@gmail.com>
2023-11-07 14:51:37 -0500

    GET request handle for login page

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const loginRoutes = require('./routes/login-routes');
 
 require('dotenv').config();
 
 // express app
 const app = express();
 
-// routes
-// req = request - info about the request
-// res = response - info about what we send back to the browser
-app.get('/', (req, res) => {
-    res.json({msg: 'Hello World'});
+// register view engine (middleware)
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
+
+// routes
+app.use('/login', loginRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
