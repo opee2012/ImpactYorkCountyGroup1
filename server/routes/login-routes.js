f94d6fd5cd8b72f648c6d57345a1684cb45c0282
opee2012 <jborth2010@gmail.com>
2023-11-15 09:37:26 -0500

    Refactor login routes to include CRUD functionality

--- server/routes/login-routes.js
+++ server/routes/login-routes.js
@@ -1,11 +1,27 @@
 const express = require('express');
-
-// controller functions
-const { login } = require('../controllers/login-controller');
+const {
+    getAllLogins,
+    getOneLogin,
+    createLogin,
+    deleteLogin,
+    updateLogin
+} = require('../controllers/login-controller');
 
 const router = express.Router();
 
-// login route
-router.get('/login', login);
+// GET all logins
+router.get('/', getAllLogins);
+
+// GET one login
+router.get('/:id', getOneLogin);
+
+// POST create login
+router.post('/', createLogin);
+
+// DELETE one login
+router.delete('/:id', deleteLogin);
+
+// PUT update one login
+router.put('/:id', updateLogin);
 
 module.exports = router;
