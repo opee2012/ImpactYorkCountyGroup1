963fa21ad943d3386fe1ad04d1125ff30a110ab4
opee2012 <jborth2010@gmail.com>
2023-11-15 09:37:07 -0500

    Add login functionality with API call

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Login = () => {
+
+    const [logins, setLogins] = useState(null);
+
+    useEffect(() => {
+        const fetchLogins = async () => {
+            const response = await fetch('/login');
+            const json = await response.json();
+
+            if (response.status === 200) {
+                setLogins(json);
+            }
+        };
+
+        fetchLogins();
+    }, []);
+
     return (
         <div className="login">
             <h1>Login</h1>
+            <div className="logins">
+                {logins && logins.map((login) => (
+                    <div key={login._id}>
+                        <h2>{login.username}</h2>
+                        <p>{login.password}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
