63e45b945134a87747b41e92ba42b8f1f6129046
opee2012 <jborth2010@gmail.com>
2023-11-16 13:01:27 -0500

    Add Login page with form and fetch for logins

---
+++ client/src/pages/Login.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+// components
+import LoginForm from "../components/login-form";
+
+// icons
+import userIcon from "../icons/user.png";
+import passwordIcon from "../icons/lock.png";
+
+// styles
+import "../styles/Login.css";
+
+
+const Login = () => {
+
+    const [login, setLogins] = useState(null);
+
+    useEffect(() => {
+        const fetchLogins = async () => {
+            const response = await fetch('/login');
+            const json = await response.json();
+
+            if (response.status === 200) {
+                setLogins(json);
+            }
+        };
+
+        fetchLogins();
+    }, []);
+
+    return (
+        <div className="container">
+            <div id="logo">
+                <img src="IYC.png" />
+            </div>
+            <div id="loginform">
+                <LoginForm userIcon={userIcon} passwordIcon={passwordIcon}/>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
