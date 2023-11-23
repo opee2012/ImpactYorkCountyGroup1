49345a87b7074797c5f68a58d2e23c54d5c81ec9
opee2012 <jborth2010@gmail.com>
2023-11-23 18:55:07 -0500

    Add http module and update server setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const loginRoutes = require('./routes/login-routes');
+const http = require('http');
 
 // express app
 const app = express();
@@ -15,23 +16,21 @@ app.use((req, res, next) => {
     next();
 });
 
-// routes
-app.use('/login', loginRoutes);
-
 // check if db is dev or prod
-let db = '';
+function MongoConnect(dbString) {
+    mongoose.connect(process.env.DB_URI + dbString)
+    let db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+        console.log(`Connected to MongoDB`);
 
-// connect to db
-function MongoConnect(db) {
-    mongoose.connect(process.env.DB_URI + db)
-    .then(() => {
-        console.log('Connected to MongoDB');
-        // listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log(`Server listening on port ${process.env.PORT}`);
+        // routes
+        require('./routes/login-routes')(app);
+        
+        http.createServer(app).listen(process.env.PORT, () => {
+            console.log(`Server is listening on port ${process.env.PORT}`);
         });
-    })
-    .catch(err => console.log(err));
+    });
 };
 
 if (process.argv[2] === 'dev') {
