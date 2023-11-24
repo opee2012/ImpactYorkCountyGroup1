fce862ee36f946e8e6ecfd3ae7d66aa443931ed6
opee2012 <jborth2010@gmail.com>
2023-11-24 13:57:41 -0500

    Remove duplicate login routes import

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const loginRoutes = require('./routes/login-routes');
 const http = require('http');
 
 // express app
