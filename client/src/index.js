3eeedea4596b5d0bab28c0a6cf438498934c41a3
opee2012 <jborth2010@gmail.com>
2023-11-27 13:47:32 -0500

    Update AuthProvider to AuthContextProvider

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
+    <AuthContextProvider>
       <App />
-    </AuthProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
