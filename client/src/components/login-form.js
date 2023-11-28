1a069665bf45be43010ffd107848bdf09e3ac957
fr677nk <fra77nk@icloud.com>
2023-11-28 13:53:55 -0500

    added login functionality for the form

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -1,83 +1,91 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLogin } from "../hooks/useLogin";
 import "../styles/loginform.css";
 
-const LoginForm = ({ userIcon , passwordIcon }) => {
-    //TODO:
-    //change font
+const LoginForm = ({ userIcon, passwordIcon }) => {
+  //TODO:
+  //change font
 
-    const [profile, setProfile] = useState({});
-    const [status, setStatus] = useState('');
-    const navigate = useNavigate();
+  const [profile, setProfile] = useState({});
+  const [status, setStatus] = useState("");
+  const { login, error, isLoading } = useLogin();
+  const navigate = useNavigate();
 
-    const handleOnChange = ({ target }) => {
+  const handleOnChange = ({ target }) => {
     const { name, value } = target;
     setProfile((prev) => ({
-        ...prev,
-        [name]: value,
+      ...prev,
+      [name]: value,
     }));
-    console.log(profile);
-    };
+    
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(profile.username, profile.password)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+      
 
-        try {
-                const response = await fetch('http://localhost:3001/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(profile),
-            });
+    // try {
+    //         const response = await fetch('http://localhost:3001/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(profile),
+    //     });
 
-            if (response.status === 200) {
-                setStatus('Login successful');
-                navigate('/upload');
-            } else {
-                setStatus('Invalid login credentials');
-            }
-        } catch (error) {
-            console.error('Error during login validation:', error);
-            setStatus('Error during login validation');
-        }
-    }
+    //     if (response.status === 200) {
+    //         setStatus('Login successful');
+    //         navigate('/upload');
+    //     } else {
+    //         setStatus('Invalid login credentials');
+    //     }
+    // } catch (error) {
+    //     console.error('Error during login validation:', error);
+    //     setStatus('Error during login validation');
+    // }
+  };
 
-    return (
+  return (
     <form onSubmit={handleSubmit}>
-        <div className="input-container">
+      <div className="input-container">
         <img src={userIcon} alt="user icon" className="icon" />
         <input
-        type="text"
-        name="username"
-        id="Username"
-        className="formtext"
-        placeholder="USERNAME"
-        value={profile.username}
-        onChange={handleOnChange}
+          type="text"
+          name="username"
+          id="Username"
+          className="formtext"
+          placeholder="USERNAME"
+          value={profile.username}
+          onChange={handleOnChange}
         />
-        </div>
-        
-        <div className="input-container">
+      </div>
+
+      <div className="input-container">
         <img src={passwordIcon} alt="password icon" className="icon" />
         <input
-        type="password"
-        name="password"
-        className="formtext"
-        placeholder="PASSWORD"
-        value={profile.password}
-        onChange={handleOnChange}
+          type="password"
+          name="password"
+          className="formtext"
+          placeholder="PASSWORD"
+          value={profile.password}
+          onChange={handleOnChange}
         />
-        </div>
-        
-        <p id="status">{status}</p>
-        <input type="submit" id="submitbtn" value="Login"></input>
-        <br />
-        <form action = "/Dashboard">
+      </div>
+
+      {error && <div>{error}</div>}
+      <button id="submitbtn">
+        Login
+      </button>
+      <br />
+      <form action="/Dashboard">
         <input type="button" id="returnbtn" value="Back To Dashboard"></input>
-        </form>
+      </form>
     </form>
-    );
+  );
 };
 
 export default LoginForm;
