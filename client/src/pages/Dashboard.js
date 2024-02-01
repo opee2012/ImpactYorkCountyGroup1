15594b3ddc93ed5752bbea685beac2c8f5c17d42
Anthony G-24 <terigillard9000@gmail.com>
2024-02-01 14:34:10 -0500

    Messing around with icons a bit.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,35 @@
 const Dashboard = () => {
     return (
         <div className="dashboard">
-            <h1>Dashboard</h1>
+            <div className="sidebar">
+                <div id="logo">
+                    <img src="IYC.png" alt="Logo" />
+                </div>
+                <h1>Menu</h1>
+                <ul>
+                    <li onClick={() => handleMenuItemClick('Coalition Information')}>
+                        <div id="information-icon">
+                            <img src="information-icon.png" alt="Information-icon" />
+                        </div>
+                        Coalition Information
+                    </li>
+                    
+                    <li onClick={() => handleMenuItemClick('Food Access')}>Food Access</li>
+                    <li onClick={() => handleMenuItemClick('Community Guidance')}>Community Guidance</li>
+                    <li onClick={() => handleMenuItemClick('Active Living')}>Active Living</li>
+                    <li onClick={() => handleMenuItemClick('Chronic Disease Prevention')}>Chronic Disease Prevention</li>
+                    <li onClick={() => handleMenuItemClick('Behavioural Health')}>Behavioural Health</li>
+                    <li onClick={() => handleMenuItemClick('Injury')}>Injury</li>
+                </ul>
+            </div>
+            <div className="content">
+                <div className="top-panel">
+                <div className="search-bar">
+                    <input type="text" placeholder="Search..." />
+                    <button type="submit">Search</button> 
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
