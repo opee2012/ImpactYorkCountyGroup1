94be43d79ca50cfe50e0f655b3c40a8ca350b0f7
morrisona4 <morrisona4@mailbox.winthrop.edu>
2024-02-02 13:56:39 -0500

    added useState for search input, added onChange function

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,46 @@
 const Dashboard = () => {
+    // State to track which menu item is selected
+    const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+
+    // State to set the text entered in the search bar
+    const [searchInput, setSearchInput] = useState(null);
+
+    // handle change and set the sate with current info
+    const handleChange = (event) => {
+        event.preventDefault();
+        setSearchInput(event.target.value);
+
+    }
+
+    // Handler function to update selected menu item
+    const handleMenuItemClick = (menuItem) => {
+        setSelectedMenuItem(menuItem);
+    };
+
+    /* 
+    // Content to be displayed based on selected menu item
+    const getContentForMenuItem = () => {
+        switch (selectedMenuItem) {
+            case "Coalition Information":
+                return <div>Coalition Information Content</div>;
+            case "Food Access":
+                return <div>Food Access Content</div>;
+            case "Community Guidance":
+                return <div>Community Guidance Content</div>;
+            case "Active Living":
+                return <div>Active Living Content</div>;
+            case "Chronic Disease Prevention":
+                return <div>Chronic Disease Prevention Content</div>;
+            case "Behavioural Health":
+                return <div>Behavioural Health Content</div>;
+            case "Injury":
+                return <div>Injury Content</div>;
+            default:
+                return <div></div>;
+        }
+    };
+    */
+
     return (
         <div className="dashboard">
             <div className="sidebar">
@@ -25,7 +67,8 @@ const Dashboard = () => {
             <div className="content">
                 <div className="top-panel">
                 <div className="search-bar">
-                    <input type="text" placeholder="Search..." />
+                    {/* Modified the input to handle change in input and to set the state with current input  */}
+                    <input type="text" placeholder="Search..."  onChange = {handleChange} value = {searchInput} />
                     <button type="submit">Search</button> 
                 </div>
             </div>
