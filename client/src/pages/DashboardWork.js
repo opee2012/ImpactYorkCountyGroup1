439ea8ac8d40c39402224050ee5775a068fbd6cb
Anthony G-24 <terigillard9000@gmail.com>
2024-02-06 02:35:56 -0500

    Small fixes

--- client/src/pages/DashboardWork.js
+++ client/src/pages/DashboardWork.js
@@ -1,17 +1,34 @@
-// Dashboard.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/DashboardRework.css'; 
 
 const Dashboard = () => {
+    // State to track which menu item is selected
+    const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+
+    // State to set the text entered in the search bar
+    const [searchInput, setSearchInput] = useState(null);
+
+    // handle change and set the sate with current info
+    const handleChange = (event) => {
+        event.preventDefault();
+        setSearchInput(event.target.value);
+
+    }
+
+    // Handler function to update selected menu item
+    const handleMenuItemClick = (menuItem) => {
+        setSelectedMenuItem(menuItem);
+    };
+
     return (
         <div className="dashboard-container">
             <header className="sidebar">
-                *Img can go here
-                *Sidebar text
+            
             </header>
 
-            <header className="top-panel"></header>
-                *Search bar
+            <header className="top-panel">
+
+            </header>
         </div>
     );
 };
