432809a225b0548d6281fb9412eb3de1b80a1dfe
Anthony G-24 <terigillard9000@gmail.com>
2024-02-05 19:24:00 -0500

    Added new addditonal dashboard page and styling file. Updated DB import to new one in App.js.

---
+++ client/src/pages/DashboardWork.js
@@ -0,0 +1,19 @@
+// Dashboard.js
+import React from 'react';
+import '../styles/DashboardRework.css'; 
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard-container">
+            <header className="sidebar">
+                *Img can go here
+                *Sidebar text
+            </header>
+
+            <header className="top-panel"></header>
+                *Search bar
+        </div>
+    );
+};
+
+export default Dashboard;
