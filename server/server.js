5761cc4cce18320239a2f1a6c3a0c6197c920a51
opee2012 <47112091+opee2012@users.noreply.github.com>
2024-02-08 14:39:02 -0500

    Upload Page (#18)
    * Add error styling to submit button
    
    * Removed return to dashboard
    
    * Update submit button style
    
    * Refactor Upload page to include authentication and
    logout functionality
    
    * upload page form skeleton
    
    * inital commit
    
    * included the logo to the top left of page
    
    * changed one letter
    
    * added comment
    
    * Delete node_modules directory
    
    We don't want node_modules folder in Github
    
    * gave upload image its own styling name. .img-upload
    
    * changed the class name for the logo. implemented temp. logout using back button
    
    * added most styling, added todolist for upload
    
    * border styling
    
    * more styling and icons
    
    ---------
    
    Co-authored-by: fr677nk <fra77nk@icloud.com>
    Co-authored-by: morrisona4 <morrisona4@winthrop.edu>

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
 
 // express app
 const app = express();
@@ -9,32 +10,31 @@ const app = express();
 // middleware
 app.use(express.json());
 
-// Logging middleware
 app.use((req, res, next) => {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] ${req.method} ${req.path}`);
+    console.log(req.path, req.method);
     next();
 });
 
-// Connect to MongoDB
+// connect to MongoDB
 function MongoConnect(dbString) {
-    mongoose.connect(process.env.DB_URI + dbString, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log('Connected to MongoDB');
+    mongoose.connect(process.env.DB_URI + dbString)
+    let db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+        console.log(`Connected to MongoDB`);
 
-            // Routes
-            require('./routes/login-routes')(app);
+        // routes
+        require('./routes/login-routes')(app);
+        
+        http.createServer(app).listen(process.env.PORT, () => {
+            console.log(`Server is listening on port ${process.env.PORT}`);
+        });
+    });
+};
 
-            const portNum = process.env.PORT;
-            app.listen(portNum, () => {
-            console.log(`Server is listening on port ${portNum}`); });
-        })
-        .catch(error => console.error('MongoDB connection error:', error));
-}
-
-// Check if db is dev or prod
+// check if db is dev or prod
 if (process.argv[2] === 'dev') {
     MongoConnect('IMPACT_DEV?retryWrites=true&w=majority');
 } else if (process.argv[2] === 'prod') {
-    MongoConnect('IMPACT_PROD?retryWrites=true&w=majority');
-}
+    db = 'IMPACT_PROD?retryWrites=true&w=majority';
+};
