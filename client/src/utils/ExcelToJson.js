ea5e5b61fe17c132fa93cc1d80f0a42088bf4749
morrisona4 <morrisona4@mailbox.winthrop.edu>
2024-02-16 13:11:54 -0500

    function to convert excel data to json

---
+++ client/src/utils/ExcelToJson.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import * as XLSX from 'xlsx';
+
+
+export function ExcelToJSON ({ file }) {
+   //const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (file) => {
+        let binaryString = file.target.result;
+        const wb = XLSX.read(binaryString, { type: 'binary' });
+
+        wb.SheetNames.forEach(function (sheetName) {
+            console.log(sheetName);
+            const XL_row_object = XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
+
+            // Initialize the formatted object for the current sheet
+            const formattedObject = [];
+
+            // Process each row object
+            XL_row_object.forEach(row => {
+                // Copy the row object
+                const rowCopy = Object.assign({}, row);
+                
+                // Remove the "__EMPTY" key from the row object
+                delete rowCopy["__EMPTY"];
+
+                // Create an object for each row with "__EMPTY" as key and corresponding value
+                const rowObject = {};
+                rowObject[row["__EMPTY"]] = rowCopy;
+                formattedObject.push(rowObject);
+            });
+
+            // Store the formatted object for the current sheet
+            const sheetData = {
+                [sheetName]: formattedObject
+            };
+
+            // Convert the object to a JSON string with indentation for readability
+            const json_object = JSON.stringify(sheetData, null, 2);
+
+            // function to add the data to the database
+
+
+
+            console.log(json_object);
+            console.log("This executes");
+        });
+    };
+    //console.log("This executes");
+    reader.readAsBinaryString(file);
+
+};
