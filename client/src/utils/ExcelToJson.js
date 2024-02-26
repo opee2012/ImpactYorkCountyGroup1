dd79246ba8e0a6bd2dbeec1c98769128c943e6c5
opee2012 <jborth2010@gmail.com>
2024-02-26 12:55:18 -0500

    tried some code

--- client/src/utils/ExcelToJson.js
+++ client/src/utils/ExcelToJson.js
@@ -53,6 +53,14 @@ export function ExcelToJSON ({ file }) {
 
             console.log(json_object);
 
+            let val = Object.values(sheetData).every((key) => {
+                Object.values(key).every((value) => {
+                    if (value.length() === 0) {
+                        console.log(key);
+                    }
+                });
+            });
+
             // Sent to server for processing
             // return json_object;
             
