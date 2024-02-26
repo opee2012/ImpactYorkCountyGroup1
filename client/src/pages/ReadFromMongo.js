6bf869b21c7f678ff82aa39cd4b1a8f4bc497ff9
Anthony G-24 <terigillard9000@gmail.com>
2024-02-26 01:23:48 -0500

    Fixed dasboard dropdown

--- client/src/pages/ReadFromMongo.js
+++ client/src/pages/ReadFromMongo.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/DashboardRework.css';
 
+import DashboardAccordion from '../components/dashboard-accordion';
+
 const Dashboard = () => {
     const [selectedMenuItem, setSelectedMenuItem] = useState(null);
     const [searchInput, setSearchInput] = useState('');
@@ -43,8 +45,22 @@ const Dashboard = () => {
                     <img src="search-icon.png" alt="" />
                 </div>
             </header>
+            <section className="content-section">
+                <DashboardAccordion data={dummyData} />
+            </section>
         </div>
     );
 };
+
+const dummyData = [
+    {
+        title: 'Suicide Deaths',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac orci at nisi suscipit eleifend sed ut dolor. Vivamus aliquet justo et fringilla consectetur. Mauris quis urna nec massa tempor semper ut at magna. Duis volutpat libero felis, ac ornare tellus pharetra sed. Proin eu dui pretium, mollis nunc fringilla, accumsan nunc. Praesent id varius lectus. Nunc elementum leo metus, nec volutpat tortor gravida eu. Maecenas ut feugiat metus.'
+    },
+    {
+        title: 'Coronary Heart Disease',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac orci at nisi suscipit eleifend sed ut dolor. Vivamus aliquet justo et fringilla consectetur. Mauris quis urna nec massa tempor semper ut at magna. Duis volutpat libero felis, ac ornare tellus pharetra sed. Proin eu dui pretium, mollis nunc fringilla, accumsan nunc. Praesent id varius lectus. Nunc elementum leo metus, nec volutpat tortor gravida eu. Maecenas ut feugiat metus.'
+    }
+];
 
 export default Dashboard;
