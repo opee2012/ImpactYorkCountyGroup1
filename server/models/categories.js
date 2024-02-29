63bf987146f8f472157fcefa89a0f886624a5786
opee2012 <47112091+opee2012@users.noreply.github.com>
2024-02-29 15:42:18 -0500

    Revert "Dash board branch (#25)"
    This reverts commit bbb39553aa7d0f43526af6375955b189ee8d8f0f.

--- server/models/categories.js
+++
@@ -1,8 +0,0 @@
-//categories.js
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-    name: String
-});
-
-module.exports = mongoose.model('category', categorySchema);
