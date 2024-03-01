3af8127dd034502486e417d1cc45f35c0c18e592
Dawson Helms <dhelm30700@gmail.com>
2024-03-01 01:17:54 -0500

    attempted rendering JSON test data to dashboard

--- client/src/components/dashboard-accordion.js
+++ client/src/components/dashboard-accordion.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect , useState } from 'react';
 import '../styles/DashboardAccordion.css'; 
 import DropdownIcon from '../icons/dropdown.png';
 import DropupIcon from '../icons/dropup.png';
-import { useDataHandle } from '../hooks/useData';
+// import { useDataHandle } from '../hooks/useData';
 
-const DashboardAccordion = () => {
-    const { error, isLoading, fetchData } = useDataHandle();
+const DashboardAccordion = ({ data }) => {
+    // const { error, isLoading, fetchData } = useDataHandle();
     const [selectedSubItem, setSelectedSubItem] = useState(null);
 
-    useEffect(() => {
+   /* useEffect(() => {
         // Fetch data when component mounts
-        fetchData(/* any necessary parameters */)
-            .then((data) => {
+        fetchData(/* any necessary parameters ) */
+          /*  .then((data) => {
                 // Update selectedSubItem state with fetched data
                 setSelectedSubItem(data);
             })
@@ -19,31 +19,51 @@ const DashboardAccordion = () => {
                 console.error('Error fetching data:', error);
             });
         }, []);
+*/
 
-    const toggleSubCategory = (i) => {
-        if (selectedSubItem == i) {
+    const toggleSubCategory = (index) => {
+        if (selectedSubItem == index) {
             return setSelectedSubItem(null);
         }
 
-        setSelectedSubItem(i);
+        setSelectedSubItem(index);
     }
 
-return(
-    <div className="wrapper">
-        <div className="accordion">
-            {data.map((item, i) => (
-                <div className="item">
-                    <div className="title" onClick={() => toggleSubCategory(i)}>
-                        <h3>{item.title}</h3>
-                        <span className="dropdownIcons">{selectedSubItem === i ? <img src={ DropupIcon } /> : <img src={ DropdownIcon } />}</span>
-                    </div>
-                    <div className={selectedSubItem === i ? 'content show' : 'content'}>{item.description}</div>
+    return (
+        <div className="wrapper">
+            <div className="accordion">
+                {data.map((category, index) => (
+                    <div key={index} className="item">
+                        <div className="title" onClick={() => toggleSubCategory(index)}>
+                            {category['Sub-Category'].map((subCategory, subIndex) => (
+                                <div key={subIndex}>
+                                    <h3>{subCategory.Name}</h3>
+                                </div>
+                            ))}
 
-                </div>
-            ))}
+                            <span className="dropdownIcons">
+                                {selectedSubItem === index ? <img src={DropupIcon} alt="Dropup Icon" /> : <img src={DropdownIcon} alt="Dropdown Icon" />}
+                            </span>
+                        </div>
+                        <div className={selectedSubItem === index ? 'content show' : 'content'}>
+                            {category['Sub-Category'].map((subCategory, subIndex) => (
+                                <div key={subIndex}>
+                                    <h4>{subCategory.Name}</h4>
+                                    <ul>
+                                        {subCategory.Data.map((item, itemIndex) => (
+                                            <li key={itemIndex}>
+                                                Year: {item.Year}, Value: {item.Value}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-)
+    );
 }
 
 export default DashboardAccordion;
