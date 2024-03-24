67dac43a0ae86cb430bbc765b1a9107f22604342
opee2012 <jborth2010@gmail.com>
2024-03-24 15:55:59 -0400

    Fixed Edit page route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import "./styles/App.css";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Upload from "./pages/Upload";
+import Edit from "./pages/Edit";
 
 function App() {
   const { state } = useAuthContext();
@@ -20,7 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/upload" element={<Upload />} />
-          <Route path="/edit/:id" element={<Upload />} />
+          <Route path="/edit/:id" element={<Edit />} />
           <Route
             path="/login"
             element={!email ? <Login /> : <Navigate to="/" />}
