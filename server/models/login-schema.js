1d617910330f842be4adca997f0da77f402be492
opee2012 <jborth2010@gmail.com>
2024-03-23 22:48:43 -0400

    Refactor signup and login methods to handle errors

--- server/models/login-schema.js
+++ server/models/login-schema.js
@@ -23,11 +23,16 @@ loginSchema.statics.hash = async function(password) {
 }
 
 // static signup method
-loginSchema.statics.signup = async function(email, password) {
+loginSchema.statics.signup = async function(email, password, admin) {
 
-    const user = await this.create({ email, password: await this.hash(password) });
+    const user = await this.create({ email, password: await this.hash(password), admin });
 
-    return user;
+    try {
+        await user.save();
+        return user;
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 // static login method
@@ -35,17 +40,21 @@ loginSchema.statics.login = async function(email, password) {
 
     const user = await this.findOne({ email: email });
 
-    if (!user) {
-        throw new Error('Invalid user');
+    try {
+        if (!user) {
+            throw new Error('Invalid email');
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            throw new Error('Invalid password');
+        };
+
+        return user;
+    } catch (err) {
+        throw new Error(err.message);
     }
-
-    const match = await bcrypt.compare(password, user.password);
-
-    if (!match) {
-        throw new Error('Invalid password');
-    };
-
-    return user;
 };
 
 module.exports.Login = mongoose.model('Login', loginSchema, 'logins');
