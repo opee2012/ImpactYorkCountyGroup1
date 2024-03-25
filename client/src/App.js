795c75732cd88367f84988bd1ab9e8663ae7df96
morrisona4 <morrisona4@mailbox.winthrop.edu>
2024-03-24 22:31:01 -0400

    rendering of password page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Upload from "./pages/Upload";
 import Edit from "./pages/Edit";
+import ChangePasswordForm from "./pages/Password";
 
 function App() {
   const { state } = useAuthContext();
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/password" element={!email ? <Login /> : <ChangePasswordForm />} />
           <Route
             path="/login"
             element={!email ? <Login /> : <Navigate to="/" />}
