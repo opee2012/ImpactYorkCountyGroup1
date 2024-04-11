5b51ecf76df43531eb32d7b0e9e2f00fae15f99a
Fr677nk <150080022+Fr677nk@users.noreply.github.com>
2024-04-11 11:29:24 -0400

    made it so dashboard wont error if no data (#61)
    * made it so dashboard wont error if no data
    
    * error checking to ensure file is xslx
    
    * local merge

--- client/src/components/dashboard-accordion.js
+++ client/src/components/dashboard-accordion.js
@@ -8,8 +8,13 @@ import { Link } from "react-router-dom";
 const DashboardAccordion = ({ category, searchInput }) => {
   const [selectedSubItems, setSelectedSubItems] = useState([]);
   const [showImages, setShowImages] = useState({});
-  let data = category.Data;
-  let categoryName = category.Category;
+  let data;
+  let categoryName;
+  if(category) {
+    data = category.Data;
+    categoryName = category.Category;
+  }
+  
   const { state } = useAuthContext();
   const { email } = state || {};
 
@@ -25,96 +30,107 @@ const DashboardAccordion = ({ category, searchInput }) => {
 
     setShowImages((prevState) => ({
       ...prevState,
-      [subCategory.Name]: !prevState[subCategory.Name]
+      [subCategory.Name]: !prevState[subCategory.Name],
     }));
   };
 
   const filteredData = data
-    .map((category) => ({
-      ...category,
-      SubCategory: category.SubCategory.filter((subCategory) => {
-        const name = subCategory.Name || "";
-        const searchText = (searchInput || "").toLowerCase();
-        return name.toLowerCase().includes(searchText);
-      }),
-    }))
-    .filter((category) => category.SubCategory.length > 0);
+    ? data
+        .map((category) => ({
+          ...category,
+          SubCategory: category.SubCategory.filter((subCategory) => {
+            const name = subCategory.Name || "";
+            const searchText = (searchInput || "").toLowerCase();
+            return name.toLowerCase().includes(searchText);
+          }),
+        }))
+        .filter((category) => category.SubCategory.length > 0)
+    : null;
+
 
   return (
     <div className="wrapper">
       <div className="wrapper-inner">
         <div className="accordion">
-          {filteredData.map((category, index) => (
-            <div key={index} className="dashboard-area">
-              <div className="subcategory">
-                <h2>{category.Key}</h2>
-              </div>
-              {category["SubCategory"].map((subCategory, subIndex) => (
-                <div key={subIndex} className="sub-category item">
-                  <div
-                    className="title"
-                    onClick={() => toggleSubCategory(subCategory)}
-                  >
-                    <h3>{subCategory.Name}</h3>
-                    <div className="edit-drop">
-                      <div className="edit-button">
-                        {email && (
-                          <Link
-                            to={`/edit/${subCategory.Name}`}
-                            state={{
-                              subCategoryData: subCategory.Data,
-                              categoryName: categoryName,
-                              categoryData: data,
-                            }}
-                          >
-                            <button>Edit</button>
-                          </Link>
-                        )}
+          {category
+            ? filteredData.map((category, index) => (
+                <div key={index} className="dashboard-area">
+                  <div className="subcategory">
+                    <h2>{category.Key}</h2>
+                  </div>
+                  {category["SubCategory"].map((subCategory, subIndex) => (
+                    <div key={subIndex} className="sub-category item">
+                      <div
+                        className="title"
+                        onClick={() => toggleSubCategory(subCategory)}
+                      >
+                        <h3>{subCategory.Name}</h3>
+                        <div className="edit-drop">
+                          <div className="edit-button">
+                            {email && (
+                              <Link
+                                to={`/edit/${encodeURIComponent(
+                                  subCategory.Name
+                                )}`}
+                                state={{
+                                  subCategoryData: subCategory.Data,
+                                  categoryName: categoryName,
+                                  categoryData: data,
+                                }}
+                              >
+                                <button>Edit</button>
+                              </Link>
+                            )}
+                          </div>
+                          <div className="drop-icons">
+                            {selectedSubItems.includes(subCategory) ? (
+                              <img src={DropupIcon} alt="Dropup Icon" />
+                            ) : (
+                              <img src={DropdownIcon} alt="Dropdown Icon" />
+                            )}
+                          </div>
+                        </div>
                       </div>
-                      <div className="drop-icons">
-                        {selectedSubItems.includes(subCategory) ? (
-                          <img src={DropupIcon} alt="Dropup Icon" />
-                        ) : (
-                          <img src={DropdownIcon} alt="Dropdown Icon" />
-                        )}
+                      <div
+                        className={`content ${
+                          selectedSubItems.includes(subCategory) ? "show" : ""
+                        }`}
+                      >
+                        <div className="content-inner">
+                          <table>
+                            <thead>
+                              <tr>
+                                <th>Year</th>
+                                <th>Value</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {subCategory.Data.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.Year}</td>
+                                  <td>{item.Value}</td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                          {showImages[subCategory.Name] && (
+                            <img
+                              id={"image-" + subCategory.Name}
+                              src={`/uploadImage/${subCategory.Name}.png`}
+                              onError={() =>
+                                document
+                                  .getElementById("image-" + subCategory.Name)
+                                  .setAttribute("hidden", true)
+                              }
+                            />
+                          )}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div
-                    className={`content ${
-                      selectedSubItems.includes(subCategory) ? "show" : ""
-                    }`}
-                  >
-                    <div className="content-inner">
-                      <table>
-                        <thead>
-                          <tr>
-                            <th>Year</th>
-                            <th>Value</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {subCategory.Data.map((item, index) => (
-                            <tr key={index}>
-                              <td>{item.Year}</td>
-                              <td>{item.Value}</td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                      {showImages[subCategory.Name] && (
-                        <img
-                          id={"image-" + subCategory.Name}
-                          src={`/uploadImage/${subCategory.Name}.png`}
-                          onError={() => document.getElementById("image-" + subCategory.Name).setAttribute("hidden", true)}
-                        />
-                      )}
-                    </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          ))}
+              ))
+            : null}
         </div>
       </div>
     </div>
